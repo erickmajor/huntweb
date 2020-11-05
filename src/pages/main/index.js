f6079d529eae9955557d1ed779349584b41a8dc5
Erick Major dos Santos <erickmajor@3227333.no-reply.drupal.org>
2020-11-04 23:29:15 -0300

    CLASS/07: Adding a better structure to display product's data in rendering process page.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from "../../services/api";
 
+import "./styles.css";
+
 export default class Main extends Component {
   state = {
       products: []
@@ -20,10 +22,18 @@ export default class Main extends Component {
 
   // This method belongs to React
   render() {
+    // Destructuring the state to get only our important data to us.
+    const { products } = this.state;
+
     return (
       <div className="product-list">
-        {this.state.products.map(product => (
-          <h2 key={product._id}>{product.title}</h2>
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <a href="#">Acessar</a>
+          </article>
         ))}
       </div>
     );
