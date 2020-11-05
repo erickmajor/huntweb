7e89a244f5847a8c564666fbb7279a9a64a17bd1
Erick Major dos Santos <erickmajor@3227333.no-reply.drupal.org>
2020-11-05 00:03:46 -0300

    CLASS/09: Adding routes to navigate to product details.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Routes from "./routes";
 
 import "./styles.css";
 
@@ -9,7 +10,7 @@ import Main from "./pages/main";
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
